bda0216e42768ff261564844020a84c20099edc7
rhoboro <rhoboro@gmail.com>
2021-05-06 11:31:35 +0900

    公式の型サンプル1

---
+++ custom/custom.c
@@ -0,0 +1,48 @@
+//
+// Created by rhoboro on 2021/05/06.
+//
+
+#define PY_SSIZE_T_CLEAN
+
+#include <Python.h>
+
+typedef struct _CustomObject {
+    PyObject_HEAD
+} CustomObject;
+
+// https://docs.python.org/ja/3/c-api/typeobj.html#pytypeobject-slots
+static PyTypeObject CustomType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+        .tp_name = "custom.Custom",
+        .tp_doc = "",
+        .tp_basicsize = sizeof(CustomObject),
+        .tp_itemsize = 0,
+        .tp_flags = Py_TPFLAGS_DEFAULT,
+        .tp_new = PyType_GenericNew,
+};
+
+// https://docs.python.org/ja/3/c-api/module.html#c.PyModuleDef
+static PyModuleDef custommodule = {
+    PyModuleDef_HEAD_INIT,
+        .m_name = "custom",
+        .m_doc = "Example module that creates an extension type.",
+        .m_size = -1,
+};
+
+PyMODINIT_FUNC
+PyInit_custom(void) {
+    PyObject *m;
+    if (PyType_Ready(&CustomType) > 0) {
+        return NULL;
+    }
+    m = PyModule_Create(&custommodule);
+    if (m == NULL)
+        return NULL;
+    Py_INCREF(&CustomType);
+    if (PyModule_AddObject(m, "Custom", (PyObject * ) & CustomType) < 0) {
+        Py_DECREF(&CustomType);
+        Py_DECREF(m);
+        return NULL;
+    }
+    return m;
+}
