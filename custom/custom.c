ed82980f520ed1388109ac7eb361c7762f5289d9
rhoboro <rhoboro@gmail.com>
2021-05-06 11:45:51 +0900

    コメントを追加

--- custom/custom.c
+++ custom/custom.c
@@ -7,18 +7,18 @@
 #include <Python.h>
 
 typedef struct _CustomObject {
-    PyObject_HEAD
+    PyObject ob_base;  // == PyObject_HEAD
 } CustomObject;
 
 // https://docs.python.org/ja/3/c-api/typeobj.html#pytypeobject-slots
 static PyTypeObject CustomType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
+    PyVarObject_HEAD_INIT(NULL, 0)  // ob_baseの初期化用ボイラープレート
         .tp_name = "custom.Custom",
-        .tp_doc = "",
-        .tp_basicsize = sizeof(CustomObject),
-        .tp_itemsize = 0,
-        .tp_flags = Py_TPFLAGS_DEFAULT,
-        .tp_new = PyType_GenericNew,
+        .tp_doc = "Custom objects",
+        .tp_basicsize = sizeof(CustomObject),  // 新しいインスタンス作成時に確保するメモリ量
+        .tp_itemsize = 0,  // listやdictなどの可変サイズオブジェクト以外は0
+        .tp_flags = Py_TPFLAGS_DEFAULT,  // Python 3.3 までに定義されているすべてのメンバを許可する
+        .tp_new = PyType_GenericNew,  // __new__ 相当
 };
 
 // https://docs.python.org/ja/3/c-api/module.html#c.PyModuleDef
@@ -32,6 +32,7 @@ static PyModuleDef custommodule = {
 PyMODINIT_FUNC
 PyInit_custom(void) {
     PyObject *m;
+    // いくつかのメンバーを適切なデフォルト値で埋めて、 Custom 型を初期化
     if (PyType_Ready(&CustomType) > 0) {
         return NULL;
     }
@@ -46,3 +47,4 @@ PyInit_custom(void) {
     }
     return m;
 }
+
